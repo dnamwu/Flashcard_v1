9b0918b998db01ed051be19585a4a9e3e0c56118
Deok Nam <dnam@wilberforce.edu>
2022-07-19 20:59:48 -0400

    Initial Commit

---
+++ Flashcard_v1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcard_v1
+//
+//  Created by Deok Nam on 7/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
