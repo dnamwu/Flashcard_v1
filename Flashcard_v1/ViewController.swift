46787ee109884364d099385a94d590e3cd276f54
Deok Nam <dnam@wilberforce.edu>
2022-07-19 21:14:17 -0400

    Add a new function load the questions and answers for the card sets

--- Flashcard_v1/ViewController.swift
+++ Flashcard_v1/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    // function for loading questions and answers to the falshcards
+    func LoadQuestionsAnswers(){
+        
+    }
 }
 
